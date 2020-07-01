3a9c17a81976f244b282dad09425c5b60a472c9f
Arya Teja <aryateja.nsa2@gmail.com>
2020-07-01 09:50:04 +0530

    send mail failed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,7 +47,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     LightboxModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule
   ],
   providers: [ProserveService],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as SESV2 from 'aws-sdk/clients/sesv2';
 
 
 @Component({
@@ -7,11 +8,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  private _ses: SESV2;
+  userName = $("#name").val();
+  userEmail = $("#email").val();
+  userPhone = $("#phone").val();
 
-
-  constructor() { }
+  constructor() { this.configSES(); }
 
   ngOnInit(): void {
+    
+  }
+  private configSES() {
+
+    this._ses = new SESV2({
+      apiVersion: '2019-09-27',
+      region: 'ap-south-1',
+    });
+  }
+  sendEmail(params) {
+    this._ses.sendEmail(params, (err, data) => {
+      if (err)
+        console.log(err);
+      else
+        console.log(data);
+    })
+
+  }
+  paramsPassing() {
+    let war = {
+      Destination: {
+        ToAddresses: ['admin@capstonereality.com']
+      },
+      Content: {
+        Simple: {
+          Body: {
+            Html: {
+              Charset: 'UTF-8',
+              Data: '<table><tr><th>Name</th><th>Email</th><th>Phone</th></tr><tr><td>' + this.userName + '</td><td>' + this.userEmail + '</td><td>' + this.userPhone + '</td></tr></table>'
+            },
+            // Text: {
+            //   Charset: 'UTF-8',
+            //   Data: 'Or you can use plain text'
+            // }
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: 'Visitor Details'
+          }
+        },
+      },
+      FromEmailAddress: 'capstonereality69@gmail.com' // Must be registered with AWS
+    };
+    this.sendEmail(war);
   }
 
+  sendit(){ 
+    this.paramsPassing;
+  }
 }
+
+
+
+
+
